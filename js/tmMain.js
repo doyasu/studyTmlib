91f1b52400d8bebfc6a967b8313dc0113aeca2b6
doyasu <ag.dys24@gmail.com>
2015-10-16 02:11:01 +0900

    画面端からプレイヤーの車がはみ出さないよう修正 操作方法の説明を追加 不要なコメントアウト削除 利用OSS、画像ソースのリンク作成

--- js/tmMain.js
+++ js/tmMain.js
@@ -4,16 +4,6 @@ var SCREEN_HEIGHT   = 400;              // スクリーン高さ
 var SCREEN_CENTER_X = SCREEN_WIDTH/2;   // スクリーン幅の半分
 var SCREEN_CENTER_Y = SCREEN_HEIGHT/2;  // スクリーン高さの半分
 
-// var PIECE_NUM_X     = 5;                // ピースの列数
-// var PIECE_NUM_Y     = 5;                // ピースの行数
-// var PIECE_NUM       = PIECE_NUM_X*PIECE_NUM_Y;  // ピース数
-// var PIECE_OFFSET_X  = 90;               // ピースオフセットX　
-// var PIECE_OFFSET_Y  = 240;              // ピースオフセットY
-// var PIECE_WIDTH     = 120;              // ピースの幅
-// var PIECE_HEIGHT    = 120;              // ピースの高さ
-
-// var FONT_FAMILY_FLAT= "'Helvetica-Light' 'Meiryo' sans-serif";  // フラットデザイン用フォント
-
 var ASSETS = {
     "player" : "./images/car.png",
     "enemy" : "./images/car2.png",
@@ -26,7 +16,7 @@ tm.main(function() {
     var app = tm.app.CanvasApp("#world");       // 生成
     app.resize(SCREEN_WIDTH, SCREEN_HEIGHT);    // サイズ(解像度)設定
     app.fitWindow();                            // 自動フィッティング有効
-    app.background = "rgba(250, 250, 250, 1.0)";// 背景色
+    app.background = "rgba(200, 200, 200, 1.0)";// 背景色
 
     var loadingScene = tm.ui.LoadingScene({
         assets: ASSETS,
@@ -80,63 +70,30 @@ tm.define("GameScene", {
         this.enemy = Enemy().addChildTo(this);
         this.enemy.setPosition(100, 100);
 
-
-
-        var self = this;
-
-        // // カレント数
-        // self.currentNumber = 1;
-        //
-        // // ピースグループ
-        // this.pieceGroup = tm.app.CanvasElement();
-        // this.addChild(this.pieceGroup);
-        //
-        // // 数字配列
-        // var nums = [].range(1, PIECE_NUM+1);  // 1~25
-        // nums.shuffle(); // シャッフル
-        //
-        // // ピースを作成
-        // for (var i=0; i<PIECE_NUM_Y; ++i) {
-        //     for (var j=0; j<PIECE_NUM_X; ++j) {
-        //         // 数値
-        //         var number = nums[ i*PIECE_NUM_X+j ];
-        //         // ピースを生成してピースグループに追加
-        //         var piece = Piece(number).addChildTo(this.pieceGroup);
-        //         // 座標を設定
-        //         piece.x = j * 125 + PIECE_OFFSET_X;
-        //         piece.y = i * 125 + PIECE_OFFSET_Y;
-        //         // タッチ時のイベントリスナーを登録
-        //         piece.onpointingstart = function() {
-        //             // 正解かどうかの判定
-        //             if (this.number === self.currentNumber) {
-        //                 // クリアかどうかの判定
-        //                 if (self.currentNumber === PIECE_NUM) {
-        //                     // 結果表示
-        //                     var time = (self.app.frame/self.app.fps)|0;
-        //                     alert("GameClear: {0}".format(time));
-        //                 }
-        //                 self.currentNumber += 1;// インクリメント
-        //                 this.disable();         // ボタン無効
-        //             }
-        //         };
-        //     }
-        // }
     },
 
     update : function(app){
         if(app.keyboard.getKey("left")){
-            this.player.x -= 8;
-            this.player.scaleX = -1;
+            if(this.player.x > PLAYER_WIDTH / 2 ){
+                this.player.x -= 8;
+                this.player.scaleX = -1;
+            }
         }
         else if(app.keyboard.getKey("right")){
-            this.player.x += 8;
-            this.player.scaleX = 1;
+            if(this.player.x < SCREEN_WIDTH - PLAYER_WIDTH / 2 ){
+                this.player.x += 8;
+                this.player.scaleX = 1;
+            }
         }
         else if(app.keyboard.getKey("up")){
-            this.player.y -= 8;
+            if(this.player.y > PLAYER_HEIGHT / 2 ){
+                this.player.y -= 8;
+            }
         }
         else if(app.keyboard.getKey("down")){
-            this.player.y += 8;
+            if(this.player.y < SCREEN_HEIGHT - PLAYER_HEIGHT / 2 ){
+                this.player.y += 8;
+            }
         }
 
         if( this.player.x > this.enemy.x-ENEMY_WIDTH &&
@@ -189,42 +146,3 @@ tm.define("CrashScene", {
     }
 
 });
-
-/*
- * ピースクラス
- */
-// tm.define("Piece", {
-//     superClass: "tm.app.Shape",
-//
-//     init: function(number) {
-//         this.superInit(PIECE_WIDTH, PIECE_HEIGHT);
-//         // 数値をセット
-//         this.number = number;
-//
-//         this.setInteractive(true);
-//         this.setBoundingType("rect");
-//
-//         var angle = tm.util.Random.randint(0, 360);
-//         this.canvas.clearColor("hsl({0}, 80%, 70%)".format(angle));
-//
-//         this.label = tm.app.Label(number).addChildTo(this);
-//         this.label
-//             .setFontSize(70)
-//             .setFontFamily(FONT_FAMILY_FLAT)
-//             .setAlign("center")
-//             .setBaseline("middle");
-//     },
-//
-//     disable: function() {
-//         this.setInteractive(false);
-//
-//         var self = this;
-//         this.tweener
-//             .clear()
-//             .to({scaleX:0}, 100)
-//             .call(function() {
-//                 self.canvas.clearColor("rgb(100, 100, 100)");
-//             }.bind(this))
-//             .to({scaleX:1, alpha:0.5}, 100);
-//     }
-// });
